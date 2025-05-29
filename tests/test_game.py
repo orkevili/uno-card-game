b4bda8b2c184b6c5f3757b521b6e19d5ef584bfa
Örkényi Vilmos <n7n1mg@uni-sopron.hu>
2025-05-29 15:25:15 +0200

    Update test_game.py

--- tests/test_game.py
+++ tests/test_game.py
@@ -1,5 +1,24 @@
 from uno import Game, Player
 
+game = Game()
+
+def test_add_player():
+    game.add_player("test")
+    assert len(game.players) == 1
+    game.add_player("test2")
+    assert len(game.players) == 2
+
+def test_pull_card():
+    """Game starts with 108 cards
+    107 in the game start, because of the starter card
+    adding a player, player starts with 7 cards so 100 left in pack.
+    pulling a card for the player so 99 cards left in the pack.
+    """
+    assert len(game.pack) == 107
+    game.add_player("test")
+    assert len(game.pack) == 100
+    game.pull_card(Player("test"))
+    assert len(game.pack) == 99
 
 def test_drop_card():
     pass
